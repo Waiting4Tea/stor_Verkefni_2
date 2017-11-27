5635ebcc3fc7475aa896f1f8311a8ae2b5e101e6
gunnni <32646538+gunnni@users.noreply.github.com>
2017-11-27 21:40:14 +0000

    Takkarnir nokkurnvegin tilbúnir
    Eins og er þá er ég bara með videoið í htmlinu til að prófa, þarf að breyta því eitthvað síðar.

--- stor_verkefni2/gogn/video_scripts.js
+++ stor_verkefni2/gogn/video_scripts.js
@@ -3,10 +3,10 @@ function vidplay() {
    var button = document.getElementById("play");
    if (video.paused) {
       video.play();
-      button.textContent = "||";
+      button.style.backgroundImage = "url('img/pause.svg')";
    } else {
       video.pause();
-      button.textContent = ">";
+      button.style.backgroundImage = "url('img/play.svg')";
    }
 }
 
@@ -18,4 +18,30 @@ function restart() {
 function skip(value) {
     var video = document.getElementById("Video1");
     video.currentTime += value;
+}
+
+function vidmute() {
+    var video = document.getElementById("Video1")
+    var button = document.getElementById("mute")
+    if (video.muted) {
+    video.muted= false;
+    button.style.backgroundImage = "url('img/mute.svg')"
+    } else {
+    video.muted= true;
+    button.style.backgroundImage = "url('img/unmute.svg')"
+    }
+}
+
+function vidfullscreen() {
+    var video = document.getElementById("Video1")
+    var button = document.getElementById("fullscreen")
+    if(video.requestFullscreen) {
+    video.requestFullscreen();
+    }
+    else if (video.mozRequestFullScreen) {
+    video.mozRequestFullScreen();
+    }
+    else if (video.webkitRequestFullscreen) {
+    video.webkitRequestFullscreen();
+    }
 }
