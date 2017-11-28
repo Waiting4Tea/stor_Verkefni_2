878081bdaa350cf5c47b1ede19124b80c5e0da67
gunnni <32646538+gunnni@users.noreply.github.com>
2017-11-28 18:43:41 +0000

    Update video_scripts.js

--- stor_verkefni2/gogn/video_scripts.js
+++ stor_verkefni2/gogn/video_scripts.js
@@ -1,3 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let API_URL = './videos.json';
+    jsonManager.fetchData(API_URL);
+});
+
+const jsonManager = (() => {
+    function fetchData(API_URL) {
+        let data;
+        const http = new XMLHttpRequest();
+
+        http.open('GET', API_URL, true);
+        http.setRequestHeader("Content-type", "application/json");
+        http.onreadystatechange = function fetchOnload() {
+            if (http.status === 200 && http.readyState === 4) {
+                data = (JSON.parse(http.response));
+                console.log(data);
+
+                // Hérna væri flott að kalla á fall sem notar data-breytuna og smíðar HTML elements
+            } else {
+                console.log('Tókst ekki að sækja gögn');
+            }
+        };
+        http.send();
+    }
+
+    return {fetchData};
+})();
+
 function vidplay() {
    var video = document.getElementById("Video1");
    var button = document.getElementById("play");
@@ -8,11 +36,6 @@ function vidplay() {
       video.pause();
       button.style.backgroundImage = "url('img/play.svg')";
    }
-}
-
-function restart() {
-    var video = document.getElementById("Video1");
-    video.currentTime = 0;
 }
 
 function skip(value) {
